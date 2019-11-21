c81c3f36df0bd8cadaf77b8b1fd820f1dc7f77d2
Ilia Pankov <stone0407@mail.ru>
2019-11-21 11:45:24 +0300

    read code!

--- src/com.neartack.lessons/one.java
+++ src/com.neartack.lessons/one.java
@@ -4,5 +4,7 @@ public class one {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println();
+
+        System.out.println("ДАВАЙ ЗАДАЧИ, ЙОБА!");
     }
 }
